ca9844b2343cee6d3d94d518a3775b747306a04f
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-04-20 07:00:45 +0530

    Update urls.py

--- bot/plugins/urls.py
+++ bot/plugins/urls.py
@@ -34,7 +34,8 @@ async def _(c, m):
 
     duration = await Utilities.get_duration(file_link)
     if isinstance(duration, str):
-        await snt.edit_text("😟 Sorry! I cannot open the file.")
+        if snt.text != "😟 Sorry! I cannot open the file.":
+            await snt.edit_text("😟 Sorry! I cannot open the file.")
         log = await m.forward(Config.LOG_CHANNEL)
         await log.reply_text(duration, True)
         return
